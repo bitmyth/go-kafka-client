4ba542c436cdbca2eb858365eb338edc8a44c67c
gaoshangheng <gaoshangheng@wisers.com>
2021-10-30 12:50:31 +0800

    group

--- run/kafka-client.go
+++ run/kafka-client.go
@@ -1,33 +1,46 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
-	"github.com/Shopify/sarama"
 	"log"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
+
+	"github.com/Shopify/sarama"
 )
 
+var topic string
+var addrs string
+var fromBeginning bool
+var group string
+
 // kafka consumer
-
 func main() {
-	var topic string
-	var addrs string
-	var offset int64
-
 	flag.StringVar(&topic, "topic", "", "input topic")
 	flag.StringVar(&addrs, "addrs", "", "input addrs,separate by comma")
-	flag.Int64Var(&offset, "offset", sarama.OffsetNewest, "input addrs,separate by comma")
+	// flag.Int64Var(&offset, "offset",sarama.OffsetNewest , "input addrs,separate by comma")
+	flag.BoolVar(&fromBeginning, "from-beginning", false, "if read from beginning")
+	flag.StringVar(&group, "group", "", "input group")
 	flag.Parse()
+
+	offset := sarama.OffsetNewest
+	if fromBeginning {
+		offset = sarama.OffsetOldest
+	}
 
 	log.Println("topic:", topic)
 	log.Println("addresses:", addrs)
 
 	addrsArray := strings.Split(addrs, ",")
 
+	if group != "" {
+		consumeGroupStart(addrsArray)
+		return
+	}
 	consumer, err := sarama.NewConsumer(addrsArray, nil)
 	if err != nil {
 		fmt.Printf("fail to start consumer, err:%v\n", err)
@@ -61,4 +74,47 @@ func main() {
 	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
+}
+
+func consumeGroupStart(addrsArray []string) {
+	consumerGroup, err := sarama.NewConsumerGroup(addrsArray, group, nil)
+	if err != nil {
+		fmt.Printf("fail to start consumer group, err:%v\n", err)
+		return
+	}
+	consumer := Consumer{
+		ready: make(chan bool),
+	}
+	consumerGroup.Consume(context.Background(), []string{topic}, &consumer)
+}
+
+// Consumer represents a Sarama consumer group consumer
+type Consumer struct {
+	ready chan bool
+}
+
+// Setup is run at the beginning of a new session, before ConsumeClaim
+func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
+	// Mark the consumer as ready
+	close(consumer.ready)
+	return nil
+}
+
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
+func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	// NOTE:
+	// Do not move the code below to a goroutine.
+	// The `ConsumeClaim` itself is called within a goroutine, see:
+	// https://github.com/Shopify/sarama/blob/main/consumer_group.go#L27-L29
+	for message := range claim.Messages() {
+		log.Printf("Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
+		session.MarkMessage(message, "")
+	}
+
+	return nil
 }
---
+++ run/kafka-sender.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/Shopify/sarama"
+	"time"
+)
+
+var config *sarama.Config
+var client sarama.SyncProducer
+var topic string
+var message string
+
+// kafka sender
+func main() {
+	client, err := Init()
+	if err != nil {
+		return
+	}
+
+	defer client.Close()
+
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.ByteEncoder(message),
+	}
+
+	// 发送消息
+	pid, offset, err := client.SendMessage(msg)
+	if err != nil {
+		fmt.Println("send msg failed, err:", err)
+		return
+	}
+	fmt.Printf("pid:%v offset:%v\n", pid, offset)
+}
+
+func Init() (sarama.SyncProducer, error) {
+	var addrs string
+	flag.StringVar(&topic, "topic", "", "input topic")
+	flag.StringVar(&addrs, "addrs", "localhost:9092", "input addrs,separate by comma")
+	flag.StringVar(&message, "message", "this is message:"+time.Now().String(), "input group")
+
+	flag.Parse()
+	log.Println("topic:", topic)
+	log.Println("addresses:", addrs)
+	log.Println("message:", message)
+
+	config = sarama.NewConfig()
+	// 发送完数据需要leader和follow都确认
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	// 成功交付的消息将在success channel返回
+	config.Producer.Return.Successes = true
+
+	addrsArray := strings.Split(addrs, ",")
+	client, err := sarama.NewSyncProducer(addrsArray, config)
+
+	if err != nil {
+		fmt.Println("producer closed, err:", err)
+		return nil, err
+	}
+	return client, nil
+}
